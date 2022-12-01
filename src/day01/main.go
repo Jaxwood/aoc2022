f925aff47a83c7373bb406aea5ceb6f9b651a6ba
Jacob Lorenzen <jacob.lorenzen@velux.com>
2022-12-01 10:14:04 +0100

    feat: solution for day01a

--- src/day01/main.go
+++ src/day01/main.go
@@ -1,5 +1,76 @@
 package main
 
-func add(a int, b int) int {
-  return a + b
+
+import (
+    "bufio"
+    "fmt"
+    "math"
+    "os"
+    "strconv"
+)
+
+type Calorie struct {
+    calories []float64
+}
+
+func read(filename string) []Calorie {
+    readFile, err := os.Open(filename)
+    defer readFile.Close()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+    fileScanner := bufio.NewScanner(readFile)
+    fileScanner.Split(bufio.ScanLines)
+
+    var fileLines []float64
+    var calories []Calorie
+
+    for fileScanner.Scan() {
+        line := fileScanner.Text()
+        if line == "" {
+            calories = append(calories, Calorie{calories: fileLines})
+            fileLines = make([]float64, 0)
+            continue
+        }
+        num, err := strconv.ParseFloat(line, 64)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fileLines = append(fileLines, num)
+    }
+    calories = append(calories, Calorie{calories: fileLines})
+
+
+    return calories
+}
+
+func day01a(filename string) float64 {
+
+    lines := read(filename)
+    var best = 0.0
+    for _, line := range lines {
+        var sum = 0.0
+        for _, calorie := range line.calories {
+            sum += calorie
+        }
+        best = math.Max(best, sum)
+    }
+
+    return best
+}
+
+func day01b(filename string) float64 {
+
+    lines := read(filename)
+    var best = 0.0
+    for _, line := range lines {
+        var sum = 0.0
+        for _, calorie := range line.calories {
+            sum += float64(calorie)
+        }
+        best = math.Max(best, sum)
+    }
+
+    return best
 }
--- src/day01/main_test.go
+++ src/day01/main_test.go
@@ -5,9 +5,17 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-    actual := add(1,2)
-    expected := 4
+func TestDay01(t *testing.T) {
+    actual := day01a("./day01.txt")
+    expected := 24000.0
+    if actual != expected {
+        t.Fatalf(`actual = %v, expected = %v`, actual, expected)
+    }
+}
+
+func TestDay01a(t *testing.T) {
+    actual := day01a("./day01a.txt")
+    expected := 69310.0
     if actual != expected {
         t.Fatalf(`actual = %v, expected = %v`, actual, expected)
     }
