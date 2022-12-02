6fcfe20787d20a2a188d6d65a7047bcd23107abe
Jacob Lorenzen <jacob.lorenzen@velux.com>
2022-12-02 15:46:20 +0100

    feat: solution for day02 part 2

--- src/day02/main.go
+++ src/day02/main.go
@@ -12,6 +12,10 @@ const ROCK_PLAYER = "X"
 const PAPER_PLAYER = "Y"
 const SCISSOR_PLAYER = "Z"
 
+const LOOSE = "X"
+const DRAW = "Y"
+const WIN = "Z"
+
 func score(round []string) int {
 	score := 0
 	hand1 := strings.TrimSpace(round[0])
@@ -43,6 +47,47 @@ func score(round []string) int {
 	return score
 }
 
+func cardToPlay(card string, outcome string) string {
+	if card == ROCK && outcome == DRAW {
+		return ROCK_PLAYER
+	}
+	if card == ROCK && outcome == LOOSE {
+		return SCISSOR_PLAYER
+	}
+	if card == ROCK && outcome == WIN {
+		return PAPER_PLAYER
+	}
+	if card == PAPER && outcome == DRAW {
+		return PAPER_PLAYER
+	}
+	if card == PAPER && outcome == LOOSE {
+		return ROCK_PLAYER
+	}
+	if card == PAPER && outcome == WIN {
+		return SCISSOR_PLAYER
+	}
+	if card == SCISSOR && outcome == DRAW {
+		return SCISSOR_PLAYER
+	}
+	if card == SCISSOR && outcome == LOOSE {
+		return PAPER_PLAYER
+	}
+	if card == SCISSOR && outcome == WIN {
+		return ROCK_PLAYER
+	}
+	panic("unknown case")
+}
+
+func scoreb(round []string) int {
+	total := 0
+	opponent := strings.TrimSpace(round[0])
+	player := strings.TrimSpace(round[1])
+
+	card := cardToPlay(opponent, player)
+
+	return total + score([]string{ opponent, card })
+}
+
 func day02a(filename string) int {
 	lines := strings.Split(filename, "\n")
 	total := 0
@@ -52,6 +97,20 @@ func day02a(filename string) int {
 		}
 		segments := strings.Split(line, " ")
 		total += score(segments)
+	}
+
+	return total
+}
+
+func day02b(filename string) int {
+	lines := strings.Split(filename, "\n")
+	total := 0
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		segments := strings.Split(line, " ")
+		total += scoreb(segments)
 	}
 
 	return total
--- src/day02/main_test.go
+++ src/day02/main_test.go
@@ -26,3 +26,19 @@ func TestDay02b(t *testing.T) {
 		t.Fatalf(`actual = %v, expected = %v`, actual, expected)
 	}
 }
+
+func TestDay02c(t *testing.T) {
+	actual := day02b(filea)
+	expected := 10334
+	if actual != expected {
+		t.Fatalf(`actual = %v, expected = %v`, actual, expected)
+	}
+}
+
+func TestDay02d(t *testing.T) {
+	actual := day02b(fileb)
+	expected := 12
+	if actual != expected {
+		t.Fatalf(`actual = %v, expected = %v`, actual, expected)
+	}
+}
