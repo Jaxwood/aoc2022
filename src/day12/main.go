c18060d07c3b3b704c5434b5f55f19397aedc832
Jacob Lorenzen <jacob.lorenzen@velux.com>
2022-12-24 09:47:13 +0100

    chore: format code

--- src/day12/main.go
+++ src/day12/main.go
@@ -17,7 +17,7 @@ func parse(file string) HeightMap {
 	result := HeightMap{}
 	for y, line := range lines {
 		for x, alt := range line {
-			result[Coord{x,y}] = int(alt)
+			result[Coord{x, y}] = int(alt)
 		}
 	}
 	return result
@@ -28,7 +28,7 @@ func findLocation(heightMap HeightMap, location rune) Coord {
 		if v == int(location) {
 			return k
 		}
-	}	
+	}
 	panic("could not locate start location")
 }
 
@@ -36,20 +36,20 @@ func neighbours(candidate Coord) []Coord {
 	x := candidate.X
 	y := candidate.Y
 	return []Coord{
-		Coord { x - 1, y },
-		Coord { x + 1, y },
-		Coord { x, y - 1 },
-		Coord { x, y + 1 },
+		Coord{x - 1, y},
+		Coord{x + 1, y},
+		Coord{x, y - 1},
+		Coord{x, y + 1},
 	}
 }
 
 func valid(heightMap HeightMap, from Coord, to Coord) bool {
-	return heightMap[to] - 1 <= heightMap[from]
+	return heightMap[to]-1 <= heightMap[from]
 }
 
 func distance(heightMap HeightMap, start Coord, end Coord) int {
 	unvisited := map[Coord]int{}
-	for k,_ := range heightMap {
+	for k, _ := range heightMap {
 		unvisited[k] = math.MaxInt32
 	}
 	unvisited[start] = 0
@@ -65,7 +65,7 @@ func distance(heightMap HeightMap, start Coord, end Coord) int {
 		locations := neighbours(next)
 		for _, loc := range locations {
 			if val, ok := unvisited[loc]; ok {
-				if move + 1 < val && valid(heightMap, next, loc) {
+				if move+1 < val && valid(heightMap, next, loc) {
 					unvisited[loc] = move + 1
 					queue = append(queue, loc)
 				}
